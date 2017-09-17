94ef54d962b936daf6048bfb7342bfb8232a1df6
David Ayers <davidaayers@gmail.com>
2017-09-17 10:36:28 -0500

    Initial spring boot application

---
+++ webapp/src/main/java/live/twitterfight/twitterfight/TwitterfightApplication.java
@@ -0,0 +1,12 @@
+package live.twitterfight.twitterfight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterfightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterfightApplication.class, args);
+	}
+}
